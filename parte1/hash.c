c0e19fd02d52fb24a40691edfb68eef30ca9d0c1
PauloRRR <ricardo.paulorr@gmail.com>
2019-08-21 23:38:25 -0300

    inicio do trabalho

---
+++ parte1/hash.c
@@ -0,0 +1,31 @@
+#include "hash.h"
+
+HASH_NODE *Table[HASH_SIZE];
+
+void hashInit() {
+  int i;
+  for (i = 0; i < HASH_SIZE; ++i) {
+    Table[i]=0;
+  }
+}
+
+int hashAddress(char *text){
+  int address = 1;
+  int i;
+  for(i=0;i<strlen(text);++i){
+    address = (address * text[i])% HASH_SIZE + 1;
+  }
+  return address - 1;
+}
+
+HASH_NODE *hashInsert(char *text){
+  HASH_NODE *newNode;
+  address = hashAddress(text);
+  newNode =(HASH_NODE*) calloc(1,sizeof(HASH_NODE));
+  newNode->type = 1;
+  newNode->text = (char*)calloc(strlen(text)+1,sizeof(char));
+  strcpy(newNode->next,text);
+  newNode->next = Table[address];
+  Table[address] = newNode;
+  return newNode;
+}
